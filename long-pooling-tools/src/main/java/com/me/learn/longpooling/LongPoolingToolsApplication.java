a7f5fdc4c204889a608ee4ffda7523dfbd49446e
Jed Li <Jed.Li@ariix.com>
2021-12-22 20:30:00 +0800

    Initial commit

---
+++ long-pooling-tools/src/main/java/com/me/learn/longpooling/LongPoolingToolsApplication.java
@@ -0,0 +1,13 @@
+package com.me.learn.longpooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongPoolingToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongPoolingToolsApplication.class, args);
+    }
+
+}
---
+++ long-pooling-tools/src/test/java/com/me/learn/longpooling/LongPoolingToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.learn.longpooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongPoolingToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
